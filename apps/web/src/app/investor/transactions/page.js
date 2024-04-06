68be97656bf6c34bfd4d5c9256766b4d9d563961
monarxxjain <jainmonark1000@gmail.com>
2024-04-07 01:48:46 +0530

    Investor UI Complete

--- apps/web/src/app/investor/transactions/page.js
+++ apps/web/src/app/investor/transactions/page.js
@@ -1,7 +1,11 @@
+"use client"
+import Transactions from "@/components/molecules/investor/Transactions";
+
 export default function Home() {
     return (
-      <div>
-        Trasaction History Page
+      <div className="h-[90vh] overflow-y-scroll bg-gray-100 px-6 py-8 flex flex-col gap-8">
+        <h1 className="text-4xl text-[rgb(6,28,55)] font-semibold mx-auto w-full text-center font-[sans-serif]">Transaction History</h1>
+        <Transactions />
       </div>
     );
 }
