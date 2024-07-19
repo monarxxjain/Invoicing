dc87fbf68d1ac7f4110accf65783d53b3690737a
monarxxjain <jainmonark1000@gmail.com>
2024-07-19 22:08:01 +0530

    Test Prod

--- apps/web/src/app/seller/page.js
+++ apps/web/src/app/seller/page.js
@@ -6,13 +6,13 @@ import jwt from 'jsonwebtoken';
 
 export default function Home() {
 
-  // const cookieStore = cookies()
-  // const token = cookieStore.get('access_token')
-  // const decodedToken = jwt.decode(token?.value);
-  // const sellerId = cookieStore.get('SELLER_ID')
-  // if(!decodedToken){
-  //   redirect("/")
-  // }
+  const cookieStore = cookies()
+  const token = cookieStore.get('access_token')
+  const decodedToken = jwt.decode(token?.value);
+  const sellerId = cookieStore.get('SELLER_ID')
+  if(!decodedToken){
+    redirect("/")
+  }
 
 
     return (
