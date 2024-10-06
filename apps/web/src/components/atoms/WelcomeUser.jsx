ef66247d2e65da8135d5af07ae7e8ee216ff665a
monarxxjain <jainmonark1000@gmail.com>
2024-10-07 04:14:55 +0530

    Testing - 8

--- apps/web/src/components/atoms/WelcomeUser.jsx
+++ apps/web/src/components/atoms/WelcomeUser.jsx
@@ -2,9 +2,20 @@
 import React from 'react'
 import { useContext, useRef } from 'react';
 import ThemeContext from '../context/ThemeContext';
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import jwt from 'jsonwebtoken';
 
 const WelcomeUser = () => {
     const { user } = useContext(ThemeContext);
+    const cookieStore = cookies()
+    const token = cookieStore.get('access_token')
+    const role = cookieStore.get('ROLE')
+    const decodedToken = jwt.decode(token?.value);
+    console.log(token, role, decodedToken)
+    if(!decodedToken?.wolleteAddr){
+      redirect("/")
+    }
   return (
     <h1 className="text-2xl">Welcome, <span className="font-serif font-semibold italic">{user?.name || "Monark Jain"}</span> 👋</h1>
   )
