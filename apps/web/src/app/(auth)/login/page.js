2b35921c7ce7a1afe7bd1fd9af8bbeae6799088a
monarxxjain <jainmonark1000@gmail.com>
2024-10-10 03:40:06 +0530

    JSON.parse fix

--- apps/web/src/app/(auth)/login/page.js
+++ apps/web/src/app/(auth)/login/page.js
@@ -1,30 +1,32 @@
-"use client"
+"use client";
 import Login from "@/components/molecules/login-signup/Login";
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
-import jwt from 'jsonwebtoken';
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import jwt from "jsonwebtoken";
 
 export default function Home() {
   const router = useRouter();
-  const [existingRole, setExistingRole] = useState(typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('ROLE')) : null )
-  const [existingEmail, setExistingEmail] = useState(typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('EMAIL')) : null )
+  const [existingRole, setExistingRole] = useState(
+    typeof window !== "undefined" ? localStorage.getItem("ROLE") : null
+  );
+  const [existingEmail, setExistingEmail] = useState(
+    typeof window !== "undefined" ? localStorage.getItem("EMAIL") : null
+  );
 
   useEffect(() => {
-    const token = sessionStorage.getItem('TOKEN');
+    const token = sessionStorage.getItem("TOKEN");
     const decodedToken = jwt.decode(token);
 
-
-    if (decodedToken?.role === 'INVESTOR') {
-      router.push('/investor');
-    } 
-    else if (decodedToken?.role === 'SELLER') {
-      router.push('/seller');
+    if (decodedToken?.role === "INVESTOR") {
+      router.push("/investor");
+    } else if (decodedToken?.role === "SELLER") {
+      router.push("/seller");
     }
   }, []);
-    
-    return (
-      <div>
-        <Login existingRole={existingRole} existingEmail={existingEmail} />
-      </div>
-    );
+
+  return (
+    <div>
+      <Login existingRole={existingRole} existingEmail={existingEmail} />
+    </div>
+  );
 }
--- apps/web/src/components/molecules/homePage/HeroSection.jsx
+++ apps/web/src/components/molecules/homePage/HeroSection.jsx
@@ -7,10 +7,10 @@ import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import jwt from 'jsonwebtoken';
 
-const HeroSection = ({setCursorVariant}) => {
+const HeroSection = ({ setCursorVariant }) => {
 
-  const [token, setToken] = useState(typeof window !== 'undefined' ? JSON.parse(sessionStorage.getItem("TOKEN")) : null)
-  const [role, setRole] = useState(typeof window !== 'undefined' ? JSON.parse(localStorage.getItem("ROLE")) : null )
+  const [token, setToken] = useState(typeof window !== 'undefined' ? (sessionStorage.getItem("TOKEN")) : null)
+  const [role, setRole] = useState(typeof window !== 'undefined' ? (localStorage.getItem("ROLE")) : null)
   const [link, setLink] = useState("/signup")
   useEffect(() => {
     const token = sessionStorage.getItem('TOKEN');
@@ -23,50 +23,50 @@ const HeroSection = ({setCursorVariant}) => {
   }, []);
 
   return (
-    <div onMouseEnter={()=>setCursorVariant("default")} className={`w-screen h-screen flex relative items-center px-40`}>
+    <div onMouseEnter={() => setCursorVariant("default")} className={`w-screen h-screen flex relative items-center px-40`}>
       <div className='flex flex-col -mt-20'>
 
         <div className='relative w-fit'>
           <SideAnimate color={"white"} />
-          <div onMouseEnter={()=>setCursorVariant("text")} onMouseLeave={()=>setCursorVariant("default")} className='text-gray-200 text-xl w-fit tracking-tight'>Maximise Profit with Invoice Discounting</div>
+          <div onMouseEnter={() => setCursorVariant("text")} onMouseLeave={() => setCursorVariant("default")} className='text-gray-200 text-xl w-fit tracking-tight'>Maximise Profit with Invoice Discounting</div>
         </div>
 
         <div className='relative w-fit'>
           <SideAnimate color={"#41CFA2"} />
-          <h1 onMouseEnter={()=>setCursorVariant("title")} onMouseLeave={()=>setCursorVariant("default")} className='text-white text-6xl font-bold max-w-[900px] leading-tight'>
+          <h1 onMouseEnter={() => setCursorVariant("title")} onMouseLeave={() => setCursorVariant("default")} className='text-white text-6xl font-bold max-w-[900px] leading-tight'>
             Increase business savings by paying your vendors early
           </h1>
         </div>
 
-        <div className='flex flex-col gap-3 mt-20 text-lg w-fit' onMouseEnter={()=>setCursorVariant("text")} onMouseLeave={()=>setCursorVariant("default")} >
-          {heroPoints.map((val, id) => 
-              <motion.div 
-                key={id} 
-                className='flex items-center gap-2 w-fit' 
-                variants={{
-                  hidden: { opacity: 0, x: 80 },
-                  visible: { opacity: 1, x: 0 }
-                }}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 0.5, delay: 0.25+(id*0.07) }}
-              >
-                <TaskAltIcon  className='text-green-500'/>
-                <p className='text-gray-300'>{val}</p>
-              </motion.div>
-            )}
+        <div className='flex flex-col gap-3 mt-20 text-lg w-fit' onMouseEnter={() => setCursorVariant("text")} onMouseLeave={() => setCursorVariant("default")} >
+          {heroPoints.map((val, id) =>
+            <motion.div
+              key={id}
+              className='flex items-center gap-2 w-fit'
+              variants={{
+                hidden: { opacity: 0, x: 80 },
+                visible: { opacity: 1, x: 0 }
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.5, delay: 0.25 + (id * 0.07) }}
+            >
+              <TaskAltIcon className='text-green-500' />
+              <p className='text-gray-300'>{val}</p>
+            </motion.div>
+          )}
         </div>
 
-        <a href={link} 
-          onMouseEnter={()=>setCursorVariant("button")} onMouseLeave={()=>setCursorVariant("default")} 
+        <a href={link}
+          onMouseEnter={() => setCursorVariant("button")} onMouseLeave={() => setCursorVariant("default")}
           className='bg-white relative w-fit text-lg text-blue-800 font-medium rounded px-6 py-2 mt-10 hover:scale-105 active:scale-90 transition-all'
         >
           <SideAnimate color={"#41CFA2"} />
           {token ? "Dashboard" : "Get Started"}
         </a>
       </div>
-      <motion.div 
-        onMouseEnter={()=>setCursorVariant("scrollBtn")} onMouseLeave={()=>setCursorVariant("default")} 
+      <motion.div
+        onMouseEnter={() => setCursorVariant("scrollBtn")} onMouseLeave={() => setCursorVariant("default")}
         className='bg-white p-3 absolute bottom-5 left-1/2 -translate-x-[50%] text-[#0A0B1F] rounded-full animate-bounce'
         variants={{
           hidden: { opacity: 0, y: -200 },
@@ -82,27 +82,27 @@ const HeroSection = ({setCursorVariant}) => {
   )
 }
 
-const SideAnimate = ({color}) => {
+const SideAnimate = ({ color }) => {
   return (
-      <motion.div
-          variants={{
-            hidden: { left: 0 },
-            visible: { left: "100%" }
-          }}
-          initial="hidden"
-          animate="visible"
-          transition={{ duration: 0.5, delay: 0.25, ease: "easeIn" }}
-          style={{
-            position: "absolute",
-            top:0 ,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            background: color,
-            zIndex:20
-          }}
-      >
-      </motion.div>
+    <motion.div
+      variants={{
+        hidden: { left: 0 },
+        visible: { left: "100%" }
+      }}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.5, delay: 0.25, ease: "easeIn" }}
+      style={{
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        background: color,
+        zIndex: 20
+      }}
+    >
+    </motion.div>
   )
 }
 
