a45559e36c199297f290e515eb6a6e7576f4e640
monarxxjain <jainmonark1000@gmail.com>
2024-10-10 06:08:15 +0530

    Added Currency Calculation Feature at Overview Page

--- apps/web/src/components/molecules/investor/InvestorStats.jsx
+++ apps/web/src/components/molecules/investor/InvestorStats.jsx
@@ -8,6 +8,9 @@ import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 import { Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const InvestorStats = () => {
     const stats = [
@@ -70,6 +73,13 @@ const InvestorStats = () => {
             changeDuration: "Last 7 days"
         }
     ]
+    const [currency, setCurrency] = useState("USD")
+    const [input, setInput] = useState(1)
+    const [value, setValue] = useState({
+        "USD": null,
+        "INR": null,
+        "EUR": null
+    })
     const balance = 12000
     const address = "0xCe52C63E5030879079c4C5B993A5EE8282a60A88"
     const formatter = new Intl.NumberFormat('en-US');
@@ -81,17 +91,53 @@ const InvestorStats = () => {
     const colorStats2 = ["text-red-500", "text-blue-950", "text-pink-500"]
     const bgColorStats2 = ["bg-red-500", "bg-blue-950", "bg-pink-500"]
 
+    const handleChange = (e) => {
+        console.log(e.target.value)
+    }
+    useEffect(() => {
+        console.log(process.env.ETH_API)
+        const fetchData = async () => {
+            try {
+              const response = await axios.get(`https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,INR,EUR&api_key${process.env.ETH_API}`);
+              console.log(response.data);
+              setValue(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+          };
+      
+          fetchData();
+    },[])
+
 
   return (
     <section className='flex flex-col gap-8 top-1/2 absolute -translate-y-[50%]'>
-        <div className='p-12 px-44 w-full flex justify-between gap-2 bg-white rounded-lg shadow items-center'>
+        <div className='p-12 px-24 w-full flex justify-between gap-2 bg-white rounded-lg shadow items-center'>
             <div>
-                <p className='text text-gray-700 font-semibold'>Wollete Balance</p>
-                <p className='text-4xl'><span className='font-mono italic'>ETH</span> {formattedNumber}</p>
+                <p className='text text-gray-700 font-semibold'>Wollete :</p>
+                <p className='text-3xl'><span className='font-mono italic'>MetaMask</span></p>
             </div>
             <div>
-                <p className='text text-gray-700 font-semibold'>Wollete Name</p>
-                <p className='text-4xl'><span className='font-mono italic'>MetaMask</span></p>
+                <p className='text text-gray-700 font-semibold'>Balance :</p>
+                <p className='text-2xl'><span className='font-mono italic text-3xl'>{balance} </span>ETH</p>
+            </div>
+            <div>
+                <p className='text text-gray-700 font-semibold'>Current Prices :</p>
+                <div className='flex gap-2 items-center'>
+                    <div className='flex gap-1 items-center'>
+                        <input className='w-28 border text-lg border-gray-400 px-2 py-1 rounded outline-none' type='number' name='eths' value={input} onChange={(e)=>setInput(e.target.value)}/> 
+                        <p className='text-gray-500 text-lg'>ETH</p>
+                    </div>
+                    <p className='text-2xl'>=</p>
+                    <div className='flex gap-2 items-center'>
+                        <div className='flex items-center gap-0.5'>
+                            <p onClick={()=>setCurrency("USD")} className={`w-7 h-7 text-center pt-[1px] hover:bg-blue-600/10 border rounded-full transition-all cursor-pointer ${currency=="USD" ? "border-blue-600 text-blue-600 bg-blue-600/10" : "border-gray-400"}`}>$</p>
+                            <p onClick={()=>setCurrency("INR")} className={`w-7 h-7 text-center pt-[1px] hover:bg-blue-600/10 border rounded-full transition-all cursor-pointer ${currency=="INR" ? "border-blue-600 text-blue-600 bg-blue-600/10" : "border-gray-400"}`}>₹</p>
+                            <p onClick={()=>setCurrency("EUR")} className={`w-7 h-7 text-center pt-[1px] hover:bg-blue-600/10 border rounded-full transition-all cursor-pointer ${currency=="EUR" ? "border-blue-600 text-blue-600 bg-blue-600/10" : "border-gray-400"}`}>€</p>
+                        </div>
+                        <div className='text-2xl'>{(value[currency])*input}</div>
+                    </div>
+                </div>
             </div>
 
             
